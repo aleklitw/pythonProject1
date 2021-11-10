55e716adbeac3b059a28149db069ee30b212ef06
aleklitw <92438459+aleklitw@users.noreply.github.com>
2021-11-10 12:21:02 +0100

    final

---
+++ prime_numbers.py
@@ -0,0 +1,20 @@
+def prime(n):
+    number = False
+    if n < 0:
+        return "please insert a natural number"
+    elif 0 <= n < 2:
+        return "number is not prime"
+    elif n == 2:
+        return "number is prime"
+    else:
+        for i in range(2, n):
+            if (n % i) == 0:
+                number = True
+                break
+    if number:
+        return "number is not prime"
+    else:
+        return "number is prime"
+
+
+print(prime(9))
