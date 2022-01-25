4b82d21fea5738b1e27ac30c3991ccb911673d36
aleklitw <92438459+aleklitw@users.noreply.github.com>
2022-01-25 23:05:43 +0100

    csv maker

--- stock_data.py
+++ stock_data.py
@@ -1,11 +1,19 @@
 
-with open("data/FB.csv", newline='') as file:
+with open("data/FB.csv") as file:
     file_reader = file.readlines()
     data = []
     for line in file_reader:
-        line_split = line.split(',')
+        line_replace = line.replace('\n', "")
+        line_split = line_replace.split(",")
         data.append(line_split)
     for row in data:
-        row.append((float(row[4])-float(row[1]))/float(row[1]))
+        if row == data[0]:
+            row.append("Change")
+        else:
+            row.append((float(row[4]) - float(row[1])) / float(row[1]))
+    #print(data)
 
-    print(data)
+with open("data/new_FB.csv", "w") as new_file:
+   for line in data:
+       new_file.write(str(line).replace('\'', '').replace('[','').replace(']', ''))
+       new_file.write("\n")
