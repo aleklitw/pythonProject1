f67cb7ee92423e9787258afcd4858a41bde0000e
aleklitw <92438459+aleklitw@users.noreply.github.com>
2022-01-26 16:57:11 +0100

    done.

--- stock_data.py
+++ stock_data.py
@@ -25,12 +25,12 @@ def calculate_change(file_path):
         file_reader = file.readlines()
         data = []
         for line in file_reader:
-            line_replace = line.replace('\n', "")
-            line_split = line_replace.split(",")
+            line_replace = line.replace('\n', '')
+            line_split = line_replace.split(',')
             data.append(line_split)
         for row in data:
             if row == data[0]:
-                row.append("Change")
+                row.append('Change')
             else:
                 row.append(round((float(row[4]) - float(row[1])) / float(row[1])*100, 2))
 
